1d1402ca35c05c6b6f2ba89162e9bf73c54d908e
erikazeng <143123644+erikazeng@users.noreply.github.com>
2024-04-22 23:23:26 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,16 +1,5 @@
 import java.util.*;
 public class RandomStringChooser
 {
- private List <String> words; 
-  public RandomStringChooser(String[]wordArray){
-    words = new ArrayList <String> ();
-    for(String w: wordArray){
-      words.add(w);
-    }
-   public String getNext(){
-    if(words.size()>0){
-    return words.remove((int)(Math.random()*words.size()));
-
-    }
-   return "NONE";
+private List<String> words; public RandomStringChooser(String[] wordArray) { words = new ArrayList<String>(); for (String singleWord : wordArray) { words.add(singleWord); } } public String getNext() { if (words.size() > 0) { return words.remove((int)(Math.random() * words.size())); } return "NONE"; }
 }
